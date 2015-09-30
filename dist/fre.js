25b071ca06d17f39bed042928607dc26419b5f47
SRCres <srcres@gmail.com>
2015-09-30 00:22:47 -0300

    Creación de shaders con elementos script.

--- dist/fre.js
+++ dist/fre.js
@@ -17,6 +17,8 @@ fre = require('../fre');
 'use strict';
 
 fre.gl = {
+  error: window.console && window.console.error ? window.console.error.bind(window.console) : function () { },
+
   /**
    * Obtiene un contexto WebGL y lo retorna.
    * @param  {HTMLCanvasElement} canvas Elemento canvas.
@@ -40,57 +42,36 @@ fre.gl = {
   },
 
   /**
-   * Crea un objeto programa y lo usa.
+   * Crea un programa.
    * @param  {WebGLRenderingContext} gl Contexto de renderizado WebGL.
-   * @param  {String} vertShaderSource Código fuente del vertex shader.
-   * @param  {String} fragShaderSource Código fuente del fragment shader.
-   * @return {WebGLProgram} Objeto programa. Si el programa no se creó
-   * o no se pudo usar retorna false.
+   * @param  {WebGLShader[]} shaders Shaders para adjuntar.
+   * @param  {Function} [errorCallback] Callback para errores.
+   * @return {WebGLProgram} Programa creado. Si falla retorna null.
    */
-  initProgram: function (gl, vertShaderSource, fragShaderSource) {
-    var program = this.createProgram(gl, vertShaderSource, fragShaderSource);
-    if (!program) {
-      console.log('Error al crear el programa.');
-      return false;
-    }
+  createProgram: function (gl, shaders, errorCallback) {
+    var errorFn = errorCallback || this.error;
 
-    gl.useProgram(program);
-
-    return program;
-  },
-
-  /**
-   * Crea un objeto programa enlazado.
-   * @param  {WebGLRenderingContext} gl Contexto de renderizado WebGL.
-   * @param  {String} vertShaderSource Código fuente del vertex shader.
-   * @param  {String} fragShaderSource Código fuente del fragment shader.
-   * @return {WebGLProgram} Objeto programa. Si falla la creación
-   * retorna null.
-   */
-  createProgram: function (gl, vertShaderSource, fragShaderSource) {
-    var vertexShader = this.createShader(gl, gl.VERTEX_SHADER, vertShaderSource),
-        fragmentShader = this.createShader(gl, gl.FRAGMENT_SHADER, fragShaderSource);
-    if (!vertexShader || !fragmentShader) {
-      return null;
-    }
-
+    // Crea el programa
     var program = gl.createProgram();
-    if (!program) {
-      return null;
-    }
 
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
+    // Adjunta los shaders al programa
+    shaders.forEach(function (shader) {
+      gl.attachShader(program, shader);
+    });
 
+    // Vincula el programa
     gl.linkProgram(program);
 
+    // Verifica el estado del vínculo
     var linked = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (!linked) {
-      var error = gl.getProgramInfoLog(program);
-      console.log('Error al enlazar programa: ' + error);
+      // Algo salió mal y obtiene el error
+      var log = gl.getProgramInfoLog(program);
+      errFn('Error al vincular el programa: ' + log);
+
+      // Elimina el programa
       gl.deleteProgram(program);
-      gl.deleteShader(fragmentShader);
-      gl.deleteShader(vertexShader);
+
       return null;
     }
 
@@ -98,29 +79,112 @@ fre.gl = {
   },
 
   /**
-   * Crea un objeto shader
+   * Crea un program con elementos scripts.
    * @param  {WebGLRenderingContext} gl Contexto de renderizado WebGL.
-   * @param  {Constant} type El tipo de objeto shader a ser creado.
-   * @param  {String} source Código fuente del shader.
-   * @return {WebGLShader} Objeto shader. Si falla la creación
-   * retorna null.
+   * @param  {String[]} ids Ids de los elementos script.
+   * @param  {Function} [errorCallback] Callback para errores.
+   * @return {WebGLProgram} Programa creado. Si falla retorna null.
    */
-  createShader: function (gl, type, source) {
-    var shader = gl.createShader(type);
+  createProgramWithScripts: function (gl, ids, errorCallback) {
+    var shaders = [];
 
-    if (!shader) {
-      console.log('Incapaz de crear shader.');
+    for (var i = 0; i < ids.length; i++) {
+      var shader = this.createShaderWithScript(gl, ids[i], errorCallback);
+
+      if (!shader) {
+        return null;
+      }
+
+      shaders.push(shader);
+    }
+
+    return this.createProgram(gl, shaders, errorCallback);
+  },
+
+  /**
+   * Crea un shader con un elemento script.
+   * @param  {WebGLRenderingContext} gl Contexto de renderizado WebGL.
+   * @param  {String} id El id del elemento script.
+   * @param  {Number} [type] Tipo de shader a ser creado.
+   * @param  {Function} [errorCallback] Callback para errores.
+   * @return {WebGLShader} Shader creado. Si falla retorna null.
+   */
+  createShaderWithScript: function (gl, id, type, errorCallback) {
+    var errorFn = errorCallback || this.error;
+
+    if (typeof type == 'function') {
+      errorCallback = type;
+      type = undefined;
+    }
+
+    // Obtiene el script del shader
+    var script = document.getElementById(id);
+
+    // ¿Existe el script?
+    if (!script) {
+      errorFn('Error: elemento script desconocido.');
       return null;
     }
 
+    var source = script.text;
+
+    // Verifica se pasó un tipo de shader
+    if (!type) {
+      // No se pasó ningún tipo y usa el tipo definido en el script
+      if (script.type == 'x-shader/x-vertex') {
+        type = gl.VERTEX_SHADER;
+      }
+
+      if (script.type == 'x-shader/x-fragment') {
+        type = gl.FRAGMENT_SHADER;
+      }
+    }
+
+    // Verifica si es un tipo conocido
+    if (type !== gl.VERTEX_SHADER && type !== gl.FRAGMENT_SHADER) {
+      errorFn('Error: tipo de shader desconocido.');
+      return null;
+    }
+
+    return this.createShader(gl, source, type, errorCallback);
+  },
+
+  /**
+   * Crea un shader.
+   * @param  {WebGLRenderingContext} gl Contexto de renderizado WebGL.
+   * @param  {String} source Fuente del shader.
+   * @param  {Number} type Tipo de shader a ser creado.
+   * @param  {Function} [errorCallback] Callback para errores.
+   * @return {WebGLShader} Shader creado. Si falla retorna null.
+   */
+  createShader: function (gl, source, type, errorCallback) {
+    var errorFn = errorCallback || this.error;
+
+    // Crea el shader
+    var shader = gl.createShader(type);
+
+    // Verifica la creación
+    if (!shader) {
+      errorFn('Incapaz de crear el shader.');
+      return null;
+    }
+
+    // Carga la fuente del shader
     gl.shaderSource(shader, source);
+
+    // Compila el shader
     gl.compileShader(shader);
 
+    // Verifica el estado de la compilación
     var compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
     if (!compiled) {
-      var error = gl.getShaderInfoLog(shader);
-      console.log('Error al compilar shader: ' + error);
+      // Algo salió mal y obtiene el error
+      var log = gl.getShaderInfoLog(shader, gl.COMPILE_STATUS);
+      errorFn('Error al compilar el shader: ' + log);
+
+      // Elimina el shader
       gl.deleteShader(shader);
+
       return null;
     }
 
